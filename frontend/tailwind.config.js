8d57330f24519c5e67de4b984d64a01240f63eb4
Lucky2366 <charanchandra1006@gmail.com>
2026-06-30 12:13:48 +0530

    UI colors changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import colors from 'tailwindcss/colors';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
@@ -6,18 +8,19 @@ export default {
     extend: {
       colors: {
         border: 'hsl(var(--border))',
+        gray: colors.slate,
         brand: {
-          50: '#eff6ff',
-          100: '#dbeafe',
-          200: '#bfdbfe',
-          300: '#93c5fd',
-          400: '#60a5fa',
-          500: '#3b82f6',
-          600: '#2563eb',
-          700: '#1d4ed8',
-          800: '#1e40af',
-          900: '#1e3a8a',
-          950: '#172554',
+          50: '#F2F8FB',
+          100: '#BDD8E9',
+          200: '#9CCBEF',
+          300: '#7BBDE8',
+          400: '#6EA2B3',
+          500: '#4E8EA2',
+          600: '#49769F',
+          700: '#265C8A',
+          800: '#0A4174',
+          900: '#001D39',
+          950: '#001124',
         },
         accent: {
           50: '#fdf4ff',
